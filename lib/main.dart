abd7593f0a2eb98eb8e58d6dc29a3beb98938a88
Javier Claros <jclaros@afp-futuro.com>
2020-01-09 09:58:22 -0400

    Stream y Provider integrados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_validation/src/bloc/provider.dart';
 import 'package:flutter_validation/src/pages/home_page.dart';
 import 'package:flutter_validation/src/pages/login_page.dart';
 
@@ -7,14 +8,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-        'home': (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+      ),
     );
   }
 }
